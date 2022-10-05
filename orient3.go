6566dcc19f3896b1765748758b0088b9de0f8a53
Neil Pankey <git@neilpa.me>
2022-10-04 21:06:56 -0700

    go: `Orient3Vec` with error bounds checks
    ```
    go test -benchmem -run=^$ -bench Orient3
    goos: darwin
    goarch: arm64
    pkg: neilpa.me/cgo-shewchuk-robust
    Benchmark_Orient3-8      	   66122	     17038 ns/op	       0 B/op	       0 allocs/op
    Benchmark_Orient3Ptr-8   	   21759	     55058 ns/op	       0 B/op	       0 allocs/op
    Benchmark_Orient3Vec-8   	   86936	     13706 ns/op	       0 B/op	       0 allocs/op
    PASS
    ok  	neilpa.me/cgo-shewchuk-robust	4.526s
    ```

--- orient3.go
+++ orient3.go
@@ -15,20 +15,47 @@ import "math"
 //
 // Each slice parameter must contain at least 3 values.
 func Orient3(a, b, c, d []float64) float64 {
-	var adx, bdx, cdx, ady, bdy, cdy, adz, bdz, cdz float64
+	pa := (*C.double)(&a[0])
+	pb := (*C.double)(&b[0])
+	pc := (*C.double)(&c[0])
+	pd := (*C.double)(&d[0])
+	return orient3(pa, pb, pc, pd,
+		a[0]-d[0], b[0]-d[0], c[0]-d[0],
+		a[1]-d[1], b[1]-d[1], c[1]-d[1],
+		a[2]-d[2], b[2]-d[2], c[2]-d[2])
+}
+
+// Orient3Vec is similiar to `Orient3` but takes a point-like struct
+// pointer rather than a slice.
+func Orient3Vec(a, b, c, d *XYZ) float64 {
+	pa := (*C.double)(&a.X)
+	pb := (*C.double)(&b.X)
+	pc := (*C.double)(&c.X)
+	pd := (*C.double)(&d.X)
+	return orient3(pa, pb, pc, pd,
+		a.X-d.X, b.X-d.X, c.X-d.X,
+		a.Y-d.Y, b.Y-d.Y, c.Y-d.Y,
+		a.Z-d.Z, b.Z-d.Z, c.Z-d.Z)
+}
+
+// Orient3Ptr is the direct wrapper of `orient3d` from `predicates.c`.
+// See `Orient3` for additional details.
+func Orient3Ptr(a, b, c, d *float64) float64 {
+	pa := (*C.double)(a)
+	pb := (*C.double)(b)
+	pc := (*C.double)(c)
+	pd := (*C.double)(d)
+	return float64(C.orient3d(pa, pb, pc, pd))
+}
+
+// orient3 implements the basic error bound checks to minimize
+// CGO calls to the adaptive implementation.
+func orient3(pa, pb, pc, pd *C.double,
+	adx, bdx, cdx, ady, bdy, cdy, adz, bdz, cdz float64) float64 {
+
 	var bdxcdy, cdxbdy, cdxady, adxcdy, adxbdy, bdxady float64
 	var det float64
 	var permanent, errbound float64
-
-	adx = a[0] - d[0]
-	bdx = b[0] - d[0]
-	cdx = c[0] - d[0]
-	ady = a[1] - d[1]
-	bdy = b[1] - d[1]
-	cdy = c[1] - d[1]
-	adz = a[2] - d[2]
-	bdz = b[2] - d[2]
-	cdz = c[2] - d[2]
 
 	bdxcdy = bdx * cdy
 	cdxbdy = cdx * bdy
@@ -54,25 +81,5 @@ func Orient3(a, b, c, d []float64) float64 {
 		return det
 	}
 
-	pa := (*C.double)(&a[0])
-	pb := (*C.double)(&b[0])
-	pc := (*C.double)(&c[0])
-	pd := (*C.double)(&d[0])
 	return float64(C.orient3dadapt(pa, pb, pc, pd, C.double(permanent)))
-}
-
-// Orient3Vec is similiar to `Orient3` but takes a point-like struct
-// pointer rather than a slice.
-func Orient3Vec(a, b, c, d *XYZ) float64 {
-	return Orient3Ptr(&a.X, &b.X, &c.X, &d.X) // TODO: go error bounds check
-}
-
-// Orient3Ptr is the direct wrapper of `orient3d` from `predicates.c`.
-// See `Orient3` for additional details.
-func Orient3Ptr(a, b, c, d *float64) float64 {
-	pa := (*C.double)(a)
-	pb := (*C.double)(b)
-	pc := (*C.double)(c)
-	pd := (*C.double)(d)
-	return float64(C.orient3d(pa, pb, pc, pd))
 }
